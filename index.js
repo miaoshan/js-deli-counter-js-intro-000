11eee009864066074d435e48d9c2d6983e73e527
miao shan <minnieshan@gmail.com>
2019-01-01 20:47:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var katzDeliLine = [];
- 
+
 function takeANumber(line,name){
 
  line.push(name);
 return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
-function nowServing(line){
+function nowServing(katzDeliLine){
 
-  
+
 }
