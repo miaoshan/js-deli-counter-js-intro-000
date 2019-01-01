65e67855c2dab3c5aba7f592f0907a880a966253
miao shan <minnieshan@gmail.com>
2019-01-01 20:54:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
 function nowServing(line){
-  if(line.length==0){
+  if(!line.length){
     return "There is nobody waiting to be served!";
   }
   else{
